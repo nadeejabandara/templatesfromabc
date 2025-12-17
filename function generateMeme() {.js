1f2f86edc2b2a44110eb81a63b6a08562241a308
nadeejabandara <mr.nadeeja@gmail.com>
2025-12-18 00:56:53 +0530

    Delete function generateMeme() {.js

--- function generateMeme() {.js
+++
@@ -1,9 +0,0 @@
-function generateMeme() {
-  let topText = document.getElementById("topText").value;
-  let bottomText = document.getElementById("bottomText").value;
-  let image = document.getElementById("memeSelect").value;
-
-  document.querySelector(".top").innerText = topText;
-  document.querySelector(".bottom").innerText = bottomText;
-  document.getElementById("memeImage").src = image;
-}
